b61e1a189234ab3cf9deb6b07e26213276956fc8
json <json@healingpaper.com>
2019-08-02 11:39:55 +0900

    Change application.properties to application.yml

--- src/main/java/com/example/sample/configuration/DatabaseConfiguration.java
+++ src/main/java/com/example/sample/configuration/DatabaseConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:/application.yml")
 public class DatabaseConfiguration {
 
   @Autowired
