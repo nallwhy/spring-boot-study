26d9c8cea7d728433aa39d69f1c4802d5ce43000
json <json@healingpaper.com>
2020-02-25 12:45:10 +0900

    Add LoggerInterceptor

---
+++ src/main/java/com/example/sample/configuration/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.sample.configuration;
+
+import com.example.sample.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/boards*");
+	}
+}
